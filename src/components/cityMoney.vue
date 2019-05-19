61a17987c5941b4ce73a9e97a04626f2346744a6
ONeal182 <40865712+ONeal182@users.noreply.github.com>
2019-05-19 16:32:09 +0400

    Перенес элементы в компоненты

---
+++ src/components/cityMoney.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="calc__rightSide__content__st__right">
+    <h2 class="calcSum" style="width: 155px">+{{SumCity.toLocaleString()}} Р</h2>
+    <span class="persent">25 % годовых</span>
+    <div
+        class="chart-cylinder chart-cylinder-green-shadow cityMoney"
+        id="cityMoney"
+        style="z-index: 1; border-radius:50%/23px ;"
+        :style = "{ height: heightDiagramCity}"
+        ></div>
+    <div
+        class="chart-cylinder chart-cylinder-white-shadow"
+        style="height: 69px; margin-top: -35px; border-radius: 50%/23px ; "></div>
+    <p style="text-align:center; padding-bottom:90px;" class="namePersent">Город Денег</p>
+</div>
+</template>
+
+<script>
+export default {
+    props:['SumCity', 'heightDiagramCity'],
+    data(){
+        return{
+            
+            }
+    }
+}
+</script>
+
+<style lang="less">
+
+</style>
+
---
+++ src/components/deposit.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="calc__rightSide__content__st__left">
+    <h2 class="calcSum"  style="width: 150px">+ {{SumNormal.toLocaleString()}} Р</h2>
+    <span class="persent">6.5 % годовых</span>
+    <div
+        class="chart-cylinder chart-cylinder-green-shadow"
+        id="normalMoney"
+        style="z-index: 1; border-radius:50% / 23px ;"
+        :style = "{ height: heightDiagramNormal}"
+        >
+     </div>
+    <div
+        class="chart-cylinder chart-cylinder-white-shadow depozit"
+        style="height: 69px; margin-top: -35px; border-radius:50% / 23px ;"
+        >
+    </div>
+    <p style="text-align:center; padding-bottom:90px;" class="namePersent">Депозит 
+        <span class="namePersent__quest">?</span>
+    </p>
+</div>
+</template>
+
+<script>
+export default {
+    props:['SumNormal', 'heightDiagramNormal'],
+    data(){
+        return {
+         
+        }
+    }   
+}
+</script>
+
+<style lang="less">
+
+</style>
+
